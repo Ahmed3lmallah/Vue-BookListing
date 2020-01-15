5a8361982dbf313419c7fdb6604a4b869fbfc39c
Elmallah <820121@cognizant.com>
2020-01-15 11:17:52 -0500

    Updated

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-            <!-- <book-item v-for="book in books" v-bind:key="book.title" :book="book"></book-item> -->
+            <book-item v-for="book in books" v-bind:key="book.title" :book="book"></book-item>
         </ul>
         <book-form @addBook='appendBook'></book-form>
     </div>
